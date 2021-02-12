74c0c5ac3d73001812f3536bcc1563c9d6f12bb1
psychome004 <anil.stephen66@gmail.com>
2021-02-12 14:36:43 +0530

    Updated breadcrumb

--- page.php
+++ page.php
@@ -7,6 +7,12 @@ get_header();
 
 	<div class="container container-single-page kl-breadcrumb">
 		<span><a class="crumb" href="<?php bloginfo('url');?>"><?php _e( 'खबर लहरिया', 'kl' ); ?></a></span><i class="fa fa-angle-right"></i>
+		<?php
+			$kl_page_parents = get_post_ancestors( get_the_ID() );
+			if( !empty( $kl_page_parents ) ): $kl_page_parents = array_reverse( $kl_page_parents );
+			foreach( $kl_page_parents as $kl_page_parent ): ?>
+				<span><a class="crumb" href="<?php echo get_permalink( $kl_page_parent ); ?>"><?php echo get_the_title( $kl_page_parent ); ?></a></span><i class="fa fa-angle-right"></i>
+			<?php endforeach; endif; ?>
 		<span><?php the_title(); ?></span>
 	</div>
 	<div class="container">

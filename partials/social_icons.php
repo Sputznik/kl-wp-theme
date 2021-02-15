5fec37d6c4bf49e244dc85df14b09f15c8ada486
psychome004 <anil.stephen66@gmail.com>
2021-02-15 16:28:14 +0530

    Updated social-icons template

--- partials/social_icons.php
+++ partials/social_icons.php
@@ -12,9 +12,11 @@
 <div class="kl-topbar-social">
   <div class="topbar-social-inner">
     <?php foreach( $icons as $slug => $icon ): if( isset( $icons_val[ $slug ] ) && $icons_val[ $slug ] ):?>
-      <?php if( $icon['label'] == 'Login' ):?>
-        <a href="<?php _e( $icons_val[ $slug ] );?>" target="_blank"><?php _e( $icon['label'] );?></a>
-      <?php else:?>
+      <?php if( $slug == 'login' ): ?>
+        <a href="<?php _e( $icons_val[ $slug ] );?>"><?php _e( $icon['label'] );?></a>
+			<?php elseif( $slug == 'mail' ): ?>
+				<a href="mailto:<?php _e( $icons_val[ $slug ] );?>"><i class="<?php _e( $icon['icon'] )?>"></i></a>
+      <?php else: ?>
         <a href="<?php _e( $icons_val[ $slug ] );?>" target="_blank"><i class="<?php _e( $icon['icon'] )?>"></i></a>
       <?php endif;?>
     <?php endif; endforeach;?>

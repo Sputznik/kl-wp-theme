6f2735f60117dee2810c07484a136464b7b3587d
psychome004 <anil.stephen66@gmail.com>
2021-02-22 14:11:00 +0530

    Updated share template

--- lib/templates/share.php
+++ lib/templates/share.php
@@ -1,30 +1,33 @@
 <?php
 
+	$url = get_the_permalink();
+	$title = get_the_title();
+
 	$icons = array(
 		'fb'  => array(
 			'title'	=> 'Facebook',
 			'icon'  => 'fa fa-facebook',
-			'url'   => 'https://www.facebook.com/sharer/sharer.php?u='.get_the_permalink()
+			'url'   => 'https://www.facebook.com/sharer/sharer.php?u='.$url
 		),
 	  'tw'  => array(
 			'title'	=> 'Twitter',
 			'icon'  => 'fa fa-twitter',
-			'url'   => 'https://twitter.com/home?status=Check%20out%20this%20article:%20'.rawurlencode( get_the_title() ).'%20-%20'.get_the_permalink()
+			'url'   => 'https://twitter.com/intent/tweet?text='.rawurlencode('Check out this article').':%20'.rawurlencode($title).'%20-%20'.$url
 		),
 		'li'  => array(
 			'title'	=> 'Linkedin',
 			'icon'	=> 'fa fa-linkedin',
-			'url'   => 'https://www.linkedin.com/shareArticle?mini=true&url='.get_the_permalink().'&title='.rawurlencode( get_the_title() )
+			'url'   => 'https://www.linkedin.com/shareArticle?mini=true&url='.rawurlencode($url).'&title='.rawurlencode($title)
 		),
 	  'wa'  => array(
 			'title'	=> 'Whatsapp',
 			'icon'	=> 'fa fa-whatsapp',
-			'url'   => "#"
+			'url'   => 'https://api.whatsapp.com/send/'.rawurlencode($title).' %0A%0A '.rawurlencode($url)
 		),
 	  'mail'  => array(
 			'title'	=> 'Email',
 			'icon'	=> 'fa fa-envelope',
-			'url'   => 'mailto:?subject='.rawurlencode( get_the_title() ).'&body='.get_the_permalink()
+			'url'   => 'mailto:?subject='.$title.'&body='.$url
 		)
 	);
 	?>
@@ -37,7 +40,7 @@
 				<a class="kl-post-like"><i class="fa fa-heart-o"></i></a>
 			<?php endif;?>
 	    <?php foreach( $icons as $icon ):?>
-	      <a target="_blank" href="<?php _e( $icon['url'] )?>">
+	      <a target="_blank" rel="nofollow" href="<?php _e( $icon['url'] )?>">
 	        <i class="<?php _e( $icon['icon'] )?>"></i>
 	        <span class="share-tooltip"><?php _e( $icon['title'], 'kl' )?></span>
 	      </a>

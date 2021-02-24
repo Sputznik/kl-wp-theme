eac6b84fb4d3e7a28ad7a3768441d37995a705a8
psychome004 <anil.stephen66@gmail.com>
2021-02-24 11:15:31 +0530

    Added sidebar for pages

--- lib/kl-hooks/kl-widgets.php
+++ lib/kl-hooks/kl-widgets.php
@@ -22,6 +22,16 @@ add_action( 'widgets_init', function(){
   ));
 
   register_sidebar( array(
+    'name' 			    => 'Sidebar for Pages',
+    'description'   => 'Appears in the pages before the pre-footer area',
+    'id' 			      => 'kl-page-sidebar',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' 	=> '</aside>',
+    'before_title' 	=> '<h3 class="widget-title">',
+    'after_title' 	=> '</h3>',
+  ));
+
+  register_sidebar( array(
     'name' 			    => 'Pre Footer',
     'id' 			      => 'pre-footer-sidebar',
     'description' 	=> 'Appears before the footer area',

bfd5d9eef091b3f6a4fc0bc7e849de38545d5537
psychome004 <anil.stephen66@gmail.com>
2021-06-17 11:34:06 +0530

    Orbit featured-slideshow template

---
+++ orbit-query/articles-featured-slideshow.php
@@ -0,0 +1,11 @@
+<div class="kl-featured-slider-wrapper">
+  <div class="container">
+    <ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.kl-post');?>"  data-auto="true" data-autotime="4000" data-speed="600" data-url="<?php _e( $atts['url'] );?>" class="kl-featured-slider slideshow">
+      <?php while( $this->query->have_posts() ) : $this->query->the_post(); ?>
+        <li class="kl-post">
+          <?php get_template_part( 'partials/featured', 'slideshow' ); ?>
+        </li>
+      <?php endwhile;?>
+    </ul>
+  </div>
+</div> <!-- Featured Wrapper -->

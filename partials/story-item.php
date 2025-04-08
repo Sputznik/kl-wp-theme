68a40aa43127cfada126f4ca0f9747f3b4905017
Steve <anil.stephen66@gmail.com>
2025-04-08 15:26:59 +0530

    Refactor

--- partials/story-item.php
+++ partials/story-item.php
@@ -1,8 +1,7 @@
 <?php
   $permalink      = get_the_permalink();
-  $thumbnail_id   = get_post_thumbnail_id( $post->ID );
-  $thumbnail      = $thumbnail_id ? wp_get_attachment_image_src( $thumbnail_id, 'full' )[0] : $thumbnail_id;
-  $background_img = !empty( $thumbnail ) ? 'style="background-image:url('.$thumbnail.');"' : "";
+  $thumbnail      = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+  $background_img = !empty( $thumbnail ) ? 'style="background-image:url('.$thumbnail[0].');"' : "";
 ?>
 <div class="orbit-thumbnail-bg" <?php _e( $background_img ); ?>>
   <a href="<?php _e( $permalink );?>"></a>
